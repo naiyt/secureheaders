550ca519c458fdf5bc041aab67cde80dc86781e0
Nate Collings <nate@natecollings.com>
2015-01-15 15:32:29 -0700

    Add failing spec
    ...or at least this *should* be failing, but is passing for some reason.

--- fixtures/rails_4_1_8/config/initializers/secure_headers.rb
+++ fixtures/rails_4_1_8/config/initializers/secure_headers.rb
@@ -1,6 +1,6 @@
 ::SecureHeaders::Configuration.configure do |config|
   config.hsts = { :max_age => 10.years.to_i.to_s, :include_subdomains => false }
-  config.x_frame_options = 'SAMEORIGIN'
+  config.x_frame_options = 'DENY'
   config.x_content_type_options = "nosniff"
   config.x_xss_protection = {:value => 1, :mode => 'block'}
   config.x_permitted_cross_domain_policies = 'none'
